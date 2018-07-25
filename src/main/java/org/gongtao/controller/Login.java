be057e05afbc5c2447b36acf8124a5ef2806b299
gogntao <Dev_GongTao@126.com>
2018-07-25 11:33:59 +0800

    增加一些spring初始化的测试，但是不好使，什么玩意儿。。

---
+++ src/main/java/org/gongtao/controller/Login.java
@@ -0,0 +1,21 @@
+package org.gongtao.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class Login {
+    /**
+     * @param model 返回的model对象.
+     * @return a random string.
+     * @throws Exception depence .
+     */
+    @RequestMapping(value = {"/login"}, method = {RequestMethod.GET})
+    @ResponseBody
+    public final String loginAction(final Model model) throws Exception {
+        final String res = "Hello world.";
+        return res;
+    }
+}
