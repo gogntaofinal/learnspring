409cff3201d8f7149df37bb8ee7f129d7944daaf
gogntao <Dev_GongTao@126.com>
2018-08-07 17:33:33 +0800

    新加入了一些东东，变得不一样了，现在重新看看

--- src/main/java/org/gongtao/controller/Login.java
+++ src/main/java/org/gongtao/controller/Login.java
@@ -1,12 +1,21 @@
 package org.gongtao.controller;
 
+import org.gongtao.service.LoginService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+
 @Controller
 public class Login {
+    /**
+     * 用户服务接口
+     */
+    @Autowired
+    private LoginService loginService;
+
     /**
      * @param password 返回的model对象.
      * @return a random string.
@@ -14,10 +23,7 @@ public class Login {
     @RequestMapping(value = "/login", method = {RequestMethod.POST})
     @ResponseBody
     public final boolean loginAction(String username, String password) throws Exception{
-        if(username.equals("root") && password.equals("root")) {
-            return true;
-        }else{
-            return false;
-        }
+        System.out.println(username + password);
+        return loginService.login(username, password);
     }
 }
--- src/main/java/org/gongtao/serviceImpl/LoginServiceImpl.java
+++ src/main/java/org/gongtao/serviceImpl/LoginServiceImpl.java
@@ -1,19 +1,38 @@
 package org.gongtao.serviceImpl;
 
+import org.gongtao.bean.User;
 import org.gongtao.bean.UserExample;
 import org.gongtao.mapper.UserMapper;
 import org.gongtao.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class LoginServiceImpl extends LoginService {
+public class LoginServiceImpl implements LoginService {
+
     @Autowired
     private UserMapper userMapper;
 
     public boolean login(String username, String password) {
+        System.out.println(username + password);
         UserExample userExample = new UserExample();
-        userExample.createCriteria();
+//        if("".equals(username) || !StringUtils.isEmpty(username))
+
+        userExample.createCriteria().andUsernameEqualTo("test1").andPasswordEqualTo("1234");
+        try{
+            List<User> cur_user = userMapper.selectByExample(userExample);
+            int userNum = cur_user.size();
+            System.out.println(" userNum is " + userNum);
+            if(userNum != 0) {
+                System.out.println("Hello world");
+                return true;
+            }
+
+        }catch (Exception e) {
+            e.getMessage();
+        }
         return false;
     }
 }
